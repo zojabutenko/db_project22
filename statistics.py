af61ba77f702543ca76321fa4ad8fe2a4b224e81
Zoia Butenko <55627796+zojabutenko@users.noreply.github.com>
2022-12-24 14:35:44 +0100

    Delete statistics.py

--- statistics.py
+++
@@ -1,6 +0,0 @@
-from app import db
-
-
-def count_average_text_length():
-    texts = db.Table['PostText']
-    result = self.query.with_entities(db.func.avg(PostText.text_length).label("average_text_length")).filter()
